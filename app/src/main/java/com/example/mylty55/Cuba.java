d9310b070a0f74a7bfe2b96b5b811ff88bf207ce
KovshovAI <a_kovshov@mail.ru>
2020-11-29 09:17:17 +0300

    One commit

---
+++ app/src/main/java/com/example/mylty55/Cuba.java
@@ -0,0 +1,14 @@
+package com.example.mylty55;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Cuba extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cuba);
+    }
+}
